d7b1d168684759003b6317f0300a533d07e00b22
KhuziamaR <krehman000@citymail.cuny.edu>
2021-05-04 00:36:32 -0400

    optimized can construct problem using memoization

--- can_construct.py
+++ can_construct.py
@@ -1,11 +1,16 @@
-def can_construct(target,word_bank):
+def can_construct(target,word_bank,memo = dict()):
+    if target in memo:
+        return memo[target]
     if target == '':
         return True
     for string in word_bank:
         if target.startswith(string):
             remainder = target[len(string)::]
-            if can_construct(remainder,word_bank) == True:
+            if can_construct(remainder,word_bank,memo) == True:
+                memo[target] = True
                 return True
+    memo[target] = False
     return False
 
 print(can_construct("abcdef",["ab","abc","cd","def","abcd"]))
+print(can_construct("skateboard",["bo","rd","ate","t","ska","sk","boar"]))
